149f13ce98d31e89ea70db99b27ef5de79aaa95e
Jacqueline Mckay H <mcpro@mckay.local>
2025-09-30 19:33:28 -0600

    feat: logout and update profile is ok - backend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcrypt";
+import cloudinary from "../lib/cloudinary.js";
 
 //SINGUP CONTROLLER
 export const signup = async (req, res) => {
@@ -81,6 +82,41 @@ export const logout = (req, res) => {
         res.status(200).json({ message: "Logged out successfully" });
     } catch (error) {
         console.log("Error in logout controller", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+//UPDATE PROFILE CONTROLLER
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePicture} = req.body;
+        const userId = req.user._id;
+
+        if (!profilePicture) {
+            return res.status(400).json({ message: "Profile picture is required" });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePicture);
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { profilePicture: uploadResponse.secure_url },
+            { new: true }
+        );
+        
+        res.status(200).json(updatedUser);; 
+        
+    } catch (error) {
+        console.log("Error in updateProfile controller", error.message);
+        res.status(500).json({ message: "Internal server error" });
+    }
+};
+
+//CHECK AUTH CONTROLLER
+export const checkAuth = (req, res) => {
+    try {
+        res.status(200).json(req.user);
+        } catch (error) {
+        console.log("Error in checkAuth controller", error.message);
         res.status(500).json({ message: "Internal server error" });
     }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import authRoutes from './routes/auth.route.js';
 import dotenv from 'dotenv';
 import connectDB from './lib/db.js';
+import cookieParser from 'cookie-parser';
 
 //we need to load the .env file
 dotenv.config();
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3001;
 //we need to use the json parser to parse the request body
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
